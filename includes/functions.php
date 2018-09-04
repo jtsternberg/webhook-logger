c00648cd46eca4debb75d6abf72eced236821f89
Justin Sternberg <justin@dsgnwrks.pro>
2018-09-04 10:07:57 -0400

    init webapp

---
+++ includes/functions.php
@@ -0,0 +1,122 @@
+<?php
+
+function stringify_output( $data, $title, $args ) {
+	if ( is_string( $data ) ) {
+		$message = $data;
+	} else {
+		$message = '';
+		foreach ( $data as $key => $value ) {
+			$message .= '<xmp>' . $key . ': ' . print_r( $value, true ) . '</xmp>';
+		}
+	}
+
+	WpDie::output( $message, $title, $args );
+}
+
+function convert_to_bytes( $toBytes ) {
+	if ( is_numeric( $toBytes ) ) {
+		// Not much we can do here.
+		return intval( $toBytes );
+	}
+
+	$rev   = strrev( $toBytes );
+	$bytes = floatval( $toBytes );
+
+	$sizes = [
+		'B',
+		'K',
+		'M',
+		'G',
+		'T',
+		'P',
+	];
+
+	$factor = 0;
+	foreach ( $sizes as $size ) {
+		if ( 0 === strpos( $rev, $size ) ) {
+			$factor = array_search( $size, $sizes );
+		}
+	}
+
+	if ( $factor > 0 ) {
+		$bytes = $bytes * pow( 1024, $factor );
+	}
+
+	return $bytes;
+}
+
+function compare_filesize( $bytes, $compare = '' ) {
+	$pre = $compare;
+	if ( ! is_numeric( $bytes ) ) {
+		if ( ! file_exists( $bytes ) ) {
+			return null;
+		}
+
+		$bytes = filesize( $bytes );
+	}
+
+	$compare = convert_to_bytes( $compare );
+
+	if ( $compare > $bytes ) {
+		return -1;
+	}
+
+	if ( $compare < $bytes ) {
+		return $bytes - $compare;
+	}
+
+	return 0;
+}
+
+function get_file_line_count( $fileName ) {
+	$linecount = 0;
+	$handle    = fopen( $fileName, 'r' );
+	while ( ! feof( $handle ) ) {
+		$line = fgets( $handle );
+		$linecount++;
+	}
+
+	fclose( $handle );
+
+	return $linecount;
+}
+
+function purge_file_contents( $fileName, $allowed = 0 ) {
+	$fileSize     = filesize( $fileName );
+	$allowedBytes = convert_to_bytes( $allowed );
+	$lineCount    = get_file_line_count( $fileName );
+	$percent      = ( $allowedBytes / $fileSize );
+	$allowedLines = floor( $percent * $lineCount );
+	$toRemove     = $lineCount - $allowedLines;
+
+	if ( ( $fileSize - $allowedBytes ) < 1000 ) {
+		// We're close enough.
+		return;
+	}
+
+	$newFile = new SplFileObject( $fileName . '-temp', 'w' );
+
+	foreach ( new LimitIterator( new SplFileObject( $fileName ), $toRemove ) as $line ) {
+		$newFile->fwrite( $line );
+	}
+
+	unlink( $fileName );
+	rename( $fileName . '-temp', $fileName );
+}
+
+/**
+ * Send a JSON response back to an Ajax request.
+ *
+ * @param mixed $response    Variable (usually an array or object) to encode as JSON,
+ *                           then print and die.
+ * @param int   $status_code The HTTP status code to output.
+ */
+function wp_send_json( $response, $status_code = null ) {
+	@header( 'Content-Type: application/json; charset=UTF-8' );
+	if ( null !== $status_code ) {
+		WpDie::statusHeader( $status_code );
+	}
+
+	echo json_encode( $response );
+	die;
+}
---
+++ public/index.php
@@ -0,0 +1,55 @@
+<?php
+
+// ini_set( 'display_errors', 0 );
+ini_set( 'log_errors', 1 );
+ini_set( 'error_log', __DIR__.'/../debug.log' );
+
+/*
+|--------------------------------------------------------------------------
+| Register The Composer Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader
+| for our application. We just need to utilize it! We'll require it
+| into the script here so that we do not have to worry about the
+| loading of any our classes "manually". Feels great to relax.
+|
+*/
+
+require __DIR__.'/../vendor/autoload.php';
+require_once __DIR__ . '/../includes/functions.php';
+
+init();
+
+function init() {
+	$requestType = empty( $_SERVER['REQUEST_METHOD'] ) ? 'GET' : $_SERVER['REQUEST_METHOD'];
+	$args = [];
+	$data = [];
+	switch ( $requestType ) {
+		case 'GET':
+			$args['wp_die_handler'] = 'stringify_output';
+			$data[ $requestType ] = $_GET;
+			break;
+
+		default:
+			$args['wp_die_handler'] = 'wp_send_json';
+			$data[ $requestType ] = json_decode( file_get_contents('php://input'), true );
+			$data['GET'] = $_GET;
+			break;
+	}
+
+	if ( ! empty( $_GET['log'] ) ) {
+		$requestsLog = __DIR__ . '/../requests.log';
+		$diff = compare_filesize( $requestsLog, '1M', 1000 );
+		if ( $diff > 1000 ) {
+			purge_file_contents( $requestsLog, '1M' );
+		} else {
+		}
+
+		$put = file_put_contents( $requestsLog, "\n[" . date('Y-m-d H:i:s') . '] '. print_r( $data, true ), FILE_APPEND );
+
+		$data['result'] = $put;
+	}
+
+	wp_die( $data, $requestType, $args );
+}
